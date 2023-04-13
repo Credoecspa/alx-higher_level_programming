1818d8b3e92cce0d0ea251b310201c866b1bf391
Credoecspa <ezejioforchiemerie708@gmail.com>
2023-04-13 14:48:39 -0700

    class to json py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -6,4 +6,4 @@ def class_to_json(obj):
     """Returns the dictionary description and represntation with
     simple data structure for JSON serialization of an object
     """
-    return obj.
+    return obj.__dict__
--- 0x0B-python-input_output/8-main_2.py
+++ 0x0B-python-input_output/8-main_2.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 MyClass = __import__('8-my_class_2').MyClass
 class_to_json = __import__('8-class_to_json').class_to_json
 
